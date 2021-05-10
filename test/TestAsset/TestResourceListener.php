10a16ea8cd38b5b3726ac8cf497ea2e944f2381d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:12:44 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/TestAsset/TestResourceListener.php
+++ test/TestAsset/TestResourceListener.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-rest for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-rest/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-rest/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\ApiTools\Rest\TestAsset;
 
 use Laminas\ApiTools\Rest\AbstractResourceListener;
